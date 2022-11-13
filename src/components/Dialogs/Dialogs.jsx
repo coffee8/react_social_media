21cd44d58e8221da4e0decb6c0ced6cca16bf7a8
Theseus <heavenlywork@outlook.com>
2022-11-13 14:09:37 +0300

    dialogs component changed

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,20 @@ import styles from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialog
+        <div className={styles.dialogs}>
+            <div className={styles.dialogItems}>
+                <div className={styles.dialog}>AHMET</div>
+                <div className={styles.dialog}>MEHMET</div>
+                <div className={styles.dialog}>CAN</div>
+            </div>
+
+
+            <div className={styles.messages}>
+                <div className={styles.message}>MERHABA</div>
+                <div className={styles.message}>HELLO</div>
+                <div className={styles.message}>PRIVET</div>
+            </div>
+
         </div>
     )
 }
