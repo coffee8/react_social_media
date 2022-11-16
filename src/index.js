5df47fb6f8328c2ff036982ab11ffb0a64f9e28f
Theseus <heavenlywork@outlook.com>
2022-11-16 21:51:59 +0300

    added store object

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import rerenderEntireTree from './render'
-import state from "./redux/state";
+import store from "./redux/state";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-rerenderEntireTree(state);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const renderEntireTree = (state) => {
+    root.render(
+        <React.StrictMode>
+            <App state={state}
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}/>
+        </React.StrictMode>
+    );
+}
+
+renderEntireTree(store.getState());
+
+store.subscribe(renderEntireTree)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,36 +1,46 @@
 //Trying to create my own Redux and implement Flux architecture before using Redux library
 
-import rerenderEntireTree from "../render";
-
-const state = {
-    dialogPage: {
-        dialogData: [{name: 'Ahmet', id: 1}, {name: 'Mehmet', id: 2}, {name: 'Can', id: 3}],
-        messageData: [{message: 'first'}, {message: 'second'}, {message: 'third'}]
+const store = {
+    _state: {
+        dialogPage: {
+            dialogData: [{name: 'Ahmet', id: 1}, {name: 'Mehmet', id: 2}, {name: 'Can', id: 3}],
+            messageData: [{message: 'first'}, {message: 'second'}, {message: 'third'}]
+        },
+        profilePage: {
+            postData:
+                [{message: 'hi', likeCount: 25, id: 1},
+                    {message: 'I am excited about react!', likeCount: 3, id: 2},
+                    {message: 'react is cool', likeCount: 123213, id: 3}],
+            newPostText: '',
+        },
     },
-    profilePage: {
-        postData:
-            [   {message: 'hi', likeCount: 25, id: 1},
-                {message: 'I am excited about react!', likeCount: 3, id: 2},
-                {message: 'react is cool', likeCount: 123213, id: 3}],
-        newPostText: '',
+
+    _callSubscriber() {},
+
+    getState() {
+        return this._state;
+    },
+
+    addPost() {
+        const newPost = {
+            id: 4,
+            message: this._state.profilePage.newPostText,
+            likeCount: 0
+        }
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+
+    updateNewPostText(postText) {
+        this._state.profilePage.newPostText = postText;
+        this._callSubscriber(this._state);
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer;
     },
 }
 
-export const addPost = () => {
-    const newPost = {
-        id: 4,
-        message: state.profilePage.newPostText,
-        likeCount: 0
-    }
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
 
-export const updateNewPostText = (postText) => {
-    state.profilePage.newPostText = postText;
-    rerenderEntireTree(state);
-}
-
-
-export default state;
+export default store;
