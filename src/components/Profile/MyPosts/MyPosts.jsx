a2568cfaf7186b15a5db55b1944f6fbccb550034
Theseus <heavenlywork@outlook.com>
2022-11-16 13:04:10 +0300

    event handlers

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,14 @@
 import Post from './Post/Post';
+import React from "react";
 
 const MyPosts = (props) => {
 
+    const newPostElement = React.createRef(); //not recommended to use
+
+    const addPost = () => {
+        const text = newPostElement.current.value;
+        alert(text);
+    }
     const posts = props.postData.map(data => <Post message={data.message} likeCount={data.likeCount}/>)
 
     return (
@@ -9,9 +16,12 @@ const MyPosts = (props) => {
             <div>My posts</div>
             <div>
                 <div>
-                    <textarea name="post" id="1" cols="15" rows="3" placeholder="your text"></textarea>
+                    <textarea name="post" id="1"
+                              cols="15" rows="3"
+                              placeholder="your text"
+                              ref={ newPostElement }></textarea>
                 </div>
-                <button>Add post</button>
+                <button onClick={ addPost }>Add post</button>
             </div>
             <div>
                 {posts}
